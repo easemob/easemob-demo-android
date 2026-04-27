bff5af5b2bb60f47603f361cb922225bebd70797
刘吉南 <neil.liu@easemob.com>
2026-04-27 14:35:52 +0800

    Update SDK version to 4.21.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 146
-        versionName = "4.20.0"
+        versionName = "4.21.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -210,12 +210,12 @@ dependencies {
     // To use Kotlin Flow and coroutines with Room, must include the room-ktx artifact in build.gradle file.
     implementation("androidx.room:room-ktx:2.5.1")
 
-    implementation("io.hyphenate:ease-chat-kit:4.20.0")
+    implementation("io.hyphenate:ease-chat-kit:4.21.0")
 //    implementation(project(mapOf("path" to ":ease-im-kit")))
 
-    implementation("io.hyphenate:chat-call-kit:4.20.0")
+    implementation("io.hyphenate:chat-call-kit:4.21.0")
 //    implementation(project(mapOf("path" to ":ease-call-kit")))
 
-    implementation("io.hyphenate:hyphenate-chat:4.20.0")
+    implementation("io.hyphenate:hyphenate-chat:4.21.0")
 //    implementation(project(mapOf("path" to ":hyphenatechatsdk")))
 }
